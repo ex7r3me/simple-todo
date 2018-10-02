f434bd5d811da2f06a410de4b809c89804d797a9
ex7r3me <siavash.ahmadpour@gmail.com>
2018-10-03 00:49:33 +0330

    new (graphql api) apollo server
    - Apollo server return hard-coded tasks

--- server/index.js
+++ server/index.js
@@ -3,18 +3,47 @@ const { ApolloServer, gql } = require("apollo-server-express");
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
+  type Task {
+    id: Int!
+    title: String
+    isDone: Boolean
+    priority: Int
+    dueDate: String
+  }
   type Query {
-    hello: String
+    tasks: [Task]
   }
 `;
 
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    hello: () => "Hello world!"
+    tasks: () => tasks
   }
 };
-
+const tasks = [
+  {
+    id: 0,
+    title: "create a Readme",
+    priority: 1,
+    isDone: false,
+    dueDate: null
+  },
+  {
+    id: 1,
+    title: "add something else",
+    priority: 0,
+    isDone: false,
+    dueDate: null
+  },
+  {
+    id: 2,
+    title: "read the specs",
+    priority: 2,
+    isDone: true,
+    dueDate: null
+  }
+];
 const server = new ApolloServer({ typeDefs, resolvers });
 
 const app = express();

20b34e7d0cad668c185227de943cc75ffb92789e
ex7r3me <siavash.ahmadpour@gmail.com>
2018-10-04 00:57:28 +0330

    refactor (server): make server modular
    - separate files for better maintenance

---
+++ server/typeDefs.js
@@ -0,0 +1,32 @@
+const { gql } = require("apollo-server-express");
+exports.typeDefs = gql`
+  type Task {
+    id: Int!
+    title: String
+    isDone: Boolean
+    priority: Int
+    dueDate: String
+  }
+  type Query {
+    tasks: [Task]
+  }
+  type Mutation {
+    createTask(
+      id: Int
+      title: String!
+      priority: Int
+      dueDate: String
+      isDone: Boolean
+    ): Task!
+    updateTask(
+      id: Int
+      title: String!
+      priority: Int
+      dueDate: String
+      isDone: Boolean
+    ): Task!
+    deleteTask(
+      id: Int
+    ): Int!
+  }
+`;
